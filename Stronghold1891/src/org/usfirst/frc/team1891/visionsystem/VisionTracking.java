878de9d749f037a0f5312d5b4e4e5efd32a0d4b2
Tyler Manning <sylsr9@gmail.com>
2016-01-18 18:27:26 -0700

    Fixing .gitignore issues

--- Stronghold1891/src/org/usfirst/frc/team1891/visionsystem/VisionTracking.java
+++ Stronghold1891/src/org/usfirst/frc/team1891/visionsystem/VisionTracking.java
@@ -1,16 +1,6 @@
 package org.usfirst.frc.team1891.visionsystem;
 
-<<<<<<< HEAD
 public class VisionTracking 
 {
-	
-	
-=======
-/**
- * @author Tyler
- *
- */
-public class VisionTracking {
->>>>>>> 75a616cf31e7a4e04963f3060c69b18b0d6870a3
 
 }
