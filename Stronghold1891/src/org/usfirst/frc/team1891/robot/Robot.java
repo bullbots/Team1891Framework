af3c0a169834017304f01873cf7e6938edf9c49d
Josh Taylor <joshtt24@gmail.com>
2016-01-23 16:18:47 -0700

    improved drive system, added compatability for multiple joysticks

--- Stronghold1891/src/org/usfirst/frc/team1891/robot/Robot.java
+++ Stronghold1891/src/org/usfirst/frc/team1891/robot/Robot.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import org.usfirst.frc.team1891.drivesystem.*;
 import org.usfirst.frc.team1891.filewriter.LogWriter;
 import org.usfirst.frc.team1891.joysticks.*;
-import static org.mockito.Mockito.*;
 
 import java.util.LinkedList;
 
@@ -26,7 +25,6 @@ public class Robot extends IterativeRobot {
 	LogWriter log;
 	JoystickControl joy;
 	DriveSystem drive;
-<<<<<<< HEAD
 
 	//mock creation
 	LinkedList mockCAN;
@@ -43,30 +41,10 @@ public class Robot extends IterativeRobot {
 		jag = new CANJaguar(2);
 		stateMachine = new MachineState();
 		drive = new DriveSystem();
+		drive.init(new CANJaguar(1), new CANJaguar(5), new CANJaguar(4), new CANJaguar(6));
 	}
 
-=======
-	Joystick joyMain;
-	int testNum;
-	boolean initCalled=false;
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	log = new LogWriter();
-    	log.appendMessageToLog("Robot init started");
-    	joy = new JoystickControl();
-        jag = new CANJaguar(2);
-        stateMachine = new MachineState();
-        drive = new DriveSystem();
-        joyMain = new Joystick(0);
-//        joy = new JoystickControl(joyMain);
-        int testNum;
-        
-    }
-    
->>>>>>> 661ece4a31331df9e26c392ab171b3460abcf0f5
+
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
 	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
@@ -117,45 +95,14 @@ public class Robot extends IterativeRobot {
 	 * This function is called periodically during operator control
 	 */
 	public void teleopPeriodic() {
-
+		joy.getData();
+		drive.moveTank(joy.vector);
 	}
 
 	/**
 	 * This function is called periodically during test mode
 	 */
 	public void testPeriodic() {
-
+		
 	}
-
-<<<<<<< HEAD
-=======
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    	if(joyMain.getRawButton(1) || testNum==0){
-    		testNum=0;
-    		org.usfirst.frc.team1891.drivesystem.Robot driveRobo = new org.usfirst.frc.team1891.drivesystem.Robot();
-    		if(!initCalled){
-    			driveRobo.teleopInit();
-    			initCalled=true;
-    		}
-    		driveRobo.teleopPeriodic();
-    	}else if(joyMain.getRawButton(2) || testNum==1){
-    		testNum=1;
-    	}else if(joyMain.getRawButton(3) || testNum==2){
-    		testNum=2;
-    	}else if(joyMain.getRawButton(4) || testNum==3){
-    		testNum=3;
-    	}
-    }
-    
->>>>>>> 661ece4a31331df9e26c392ab171b3460abcf0f5
 }
