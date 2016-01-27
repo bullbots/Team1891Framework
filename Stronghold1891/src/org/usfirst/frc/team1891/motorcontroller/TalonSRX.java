9f4265c1136392a777d1cb1555e04ccea8270b1e
EdgarAlenPoe <ebot2000@yahoo.com>
2016-01-26 19:35:17 -0700

    created initializing and running code for percentage mode with and without PID, postion mode, and voltage mode with and without PID

--- Stronghold1891/src/org/usfirst/frc/team1891/motorcontroller/TalonSRX.java
+++ Stronghold1891/src/org/usfirst/frc/team1891/motorcontroller/TalonSRX.java
@@ -12,12 +12,15 @@ import edu.wpi.first.wpilibj.CANTalon;
 public class TalonSRX {
 	
 	int codesPerRev;
+	CANTalon talon;
 
 	/**
+	 * @param talon 
 	 * 
 	 */
-	public TalonSRX()
+	public TalonSRX(CANTalon talon)
 	{
+		this.talon = talon;
 		codesPerRev = 270;
 	}
 	
@@ -30,38 +33,104 @@ public class TalonSRX {
 	}
 	
 	/**
-	 * @param talon
+	 * 
 	 */
-	public void initPercentPID(CANTalon talon)
+	public void initPercentPID()
 	{
-		talon.setControlMode(1);
-		talon.enable();
+		talon.setControlMode(0);
+		talon.configEncoderCodesPerRev(codesPerRev);
+		talon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		talon.enableControl();
 	}
 	
 	/**
-	 * @param talon
+	 * 
 	 * @param power
 	 */
-	public void setPercentagePID(CANTalon talon, double power)
+	public void setPercentagePID(double power)
 	{
 		talon.set(power);
 	}
 	
 	/**
-	 * @param talon
+	 * 
 	 */
-	public void initPercent(CANTalon talon)
+	public void initPercent()
 	{
-		talon.setControlMode(1);
-		talon.enable();
+		talon.setControlMode(0);
+		talon.enableControl();
 	}
 	
 	/**
-	 * @param talon
+	 *
 	 * @param power
 	 */
-	public void setPercentage(CANTalon talon, double power)
+	public void setPercentage(double power)
 	{
 		talon.set(power);
+	}
+	
+	/** 
+	 * Sets specified jag to position mode with PID and enables control on it.
+	 * @param jag specifies the jag to be set
+	 * 
+	 */
+	public void initPositionPID()
+	{
+		talon.setControlMode(1);
+		talon.configEncoderCodesPerRev(codesPerRev);
+		talon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		talon.setPID(0.1, 0.01, 0);
+		talon.enableControl();
+	}
+	
+	/** 
+	 * Sets a postion for the talon to move motor to.
+	 * @param postion specifies a position for a talon to be set to
+	 * 
+	 */
+	public void setPositionPID(double postion)
+	{
+		talon.set(postion);;
+	}
+	
+	/**
+	 * Sets specified talon to voltage mode with PID and enables it.
+	 * @param jag specifies the talon to be set
+	 * 
+	 */
+	public void initVoltagePID()
+	{
+		talon.setControlMode(4);
+		talon.configEncoderCodesPerRev(codesPerRev);
+		talon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		talon.enableControl();
+	}
+	
+	/**
+	 * Sets the voltage output for the specified talon.
+	 * @param volt specifies a voltage to set motor to be set
+	 */
+	public void setVoltagePID(double volt)
+	{
+		talon.set(volt);
+	}
+	
+	/**
+	 * Sets specified talon to voltage mode without PID and enables it.
+	 */
+	public void initVoltage()
+	{
+		talon.setControlMode(4);
+		talon.enableControl();
+	}
+	
+	/**
+	 * Sets the voltage output for the specified talon.
+	 * @param volt specifies a voltage to set motor to be set
+	 */
+	public void setVoltage(double volt)
+	{
+		talon.set(volt);
 	}
 }
