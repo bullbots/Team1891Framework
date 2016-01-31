af57bf7b1625b97559ef68ae310231f51c63c481
Josh Taylor <joshtt24@gmail.com>
2016-01-30 18:40:35 -0700

    added support for dual joysticks

--- Stronghold1891/src/org/usfirst/frc/team1891/joysticks/JoystickControl.java
+++ Stronghold1891/src/org/usfirst/frc/team1891/joysticks/JoystickControl.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1891.joysticks;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Joystick.RumbleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 import org.usfirst.frc.team1891.filewriter.LogWriter;
 /**
@@ -19,8 +20,9 @@ public class JoystickControl {
 	boolean on = false;
 	int aCount = 0;
 	Timer time = new Timer();
-	
-	Joystick Stick;
+
+	Joystick Stick1;
+	Joystick Stick2;
 	/**
 	 * JoyVector contains all of the joystick data we might need
 	 */
@@ -36,22 +38,27 @@ public class JoystickControl {
 			System.out.println("Not a supported controller!");
 			
 		break;
-		//xbox 360
+		//xbox
 		case 1:
-			x = axis(0);
-			y = axis(1);
+			x = joy1Axis(0);
+			y = joy1Axis(1);
 			z = 0;
 			angle = getAngle(x, -y);
-			System.out.println(angle);
 		break;
-		//Logitech Attack
+		//single Logitech Attack
 		case 2:
-			x = axis(0);
-			y = axis(1);
+			x = joy1Axis(0);
+			y = joy1Axis(1);
 			z = 0;
 			angle = getAngle(x,-y);
-			System.out.println(angle);
 		break;
+		
+		case 3:
+			
+			x = (joy1Axis(1)-joy2Axis(1))/2;
+			y = (joy1Axis(1)+joy2Axis(1))/2;
+			z = 0;
+			angle = getAngle(x,-y);
 		
 		}
 		return vector = new JoyVector(x, y, z, angle);
@@ -60,19 +67,30 @@ public class JoystickControl {
 	/**
 	 * initiates the joystick
 	 * @param Joy
+	 * @param Joy2 Second joystick for two hands
 	 */
+	public void init(Joystick Joy, Joystick Joy2)
+	{
+		Stick1 = Joy;
+		Stick2 = Joy2;
+	}
 	public void init(Joystick Joy)
 	{
-		Stick = Joy;
+		Stick1 = Joy;
 	}
 	
 	/**
 	 * @param axis is the axis you want to read
 	 * @return value for an axis
 	 */
-	public double axis(int axis)
+	public double joy1Axis(int axis)
 	{
-		if (Math.abs(Stick.getRawAxis(axis))>DEADZONE)return Stick.getRawAxis(axis);
+		if (Math.abs(Stick1.getRawAxis(axis))>DEADZONE)return Stick1.getRawAxis(axis);
+		return 0;
+	}
+	public double joy2Axis(int axis)
+	{
+		if (Math.abs(Stick2.getRawAxis(axis))>DEADZONE)return Stick2.getRawAxis(axis);
 		return 0;
 	}
 	
@@ -83,10 +101,10 @@ public class JoystickControl {
 	 */
 	public double getAngle(double xcom, double ycom){
 		double angle = Math.PI/2;
-		if(Math.abs(xcom)<0.01&&ycom>.01){
+		if(Math.abs(xcom)<0.01&&ycom>DEADZONE){
 			return 90;
 		}
-		if(Math.abs(xcom)<0.01&&ycom<-.01){
+		if(Math.abs(xcom)<0.01&&ycom<-DEADZONE){
 			return 270;
 		}
 		if(Math.abs(ycom)<0.01&&xcom<-.2){
@@ -115,7 +133,7 @@ public class JoystickControl {
 	 */
 	public boolean button(int button)
 	{
-		return Stick.getRawButton(button);
+		return Stick1.getRawButton(button);
 	}
 	
 	/**
@@ -124,13 +142,21 @@ public class JoystickControl {
 	 */
 	public int getProfile()
 	{
-		//sets the profile to be Xbox 360
-		if(Stick.getAxisCount() == 6 && Stick.getButtonCount() == 10)
+		//sets the profile to be Xbox (360 or One)
+		
+		
+		if(Stick1.getAxisCount() == 6 && (Stick1.getButtonCount() == 10||Stick1.getButtonCount() == 11))
 		{
 			return 1;
 		}
-		//sets the profile to be Logitech Attack
-		else if(Stick.getAxisCount() == 3 && Stick.getButtonCount() == 11)
+		//sets the profile to be dual Logitech Attack
+		else if(Stick1.getAxisCount() == 3 && Stick1.getButtonCount() == 11 && Stick2.getAxisCount() == 3 && Stick2.getButtonCount() == 11)
+		{
+			return 3;
+		}
+		
+		//sets the profile to be single Logitech Attack
+		else if(Stick1.getAxisCount() == 3 && Stick1.getButtonCount() == 11)
 		{
 			return 2;
 		}
@@ -173,12 +199,12 @@ public class JoystickControl {
 	 */
 	public void setRumble(boolean isOn){
 		if(isOn){
-			Stick.setRumble(RumbleType.kLeftRumble, 1);
-			Stick.setRumble(RumbleType.kRightRumble, 1);
+			Stick1.setRumble(RumbleType.kLeftRumble, 1);
+			Stick1.setRumble(RumbleType.kRightRumble, 1);
 		}
 		if(!isOn){
-			Stick.setRumble(RumbleType.kLeftRumble, 0);
-			Stick.setRumble(RumbleType.kRightRumble, 0);
+			Stick1.setRumble(RumbleType.kLeftRumble, 0);
+			Stick1.setRumble(RumbleType.kRightRumble, 0);
 		}
 	}
 }
