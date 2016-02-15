5a4129ef7eb323c5ddf4a7fab76c23fb0c9f55b6
EdgarAlenPoe <ebot2000@yahoo.com>
2016-02-15 13:26:28 -0700

    pattern changes

--- WorkingCode/WorkingCode.ino
+++ WorkingCode/WorkingCode.ino
@@ -75,7 +75,108 @@ void loop() {
         strip.show();
         }
         break;
-      //first autonomous state
+      //auto driving state
+      case 1:
+        for (int x=0; x<=30; x++)
+        {
+          
+        }
+        break;
+      //finished auto state
+      case 2:
+        for (int x=0; x<=30; x++)
+        {
+          down(x, strip.Color(0,0,127), 20);
+        }
+        strip.show();
+        for (int x=0; x<=30; x++)
+        {
+          back(x, strip.Color(0,0,127), 20);
+        }
+        strip.show();
+        break;
+      //teleop driving state
+      case 3:
+        for (int x=0; x<=data[1]; x++)
+        {
+          strip.setPixelColor(x, colorFade(x,0,0,127,0,0,10));
+        }
+        break;
+      //collecting state
+      case 4:
+        for (int x=0; x<=30; x++)
+        {
+          for (int i=0; i<=30; i++) {
+            int q = i+x;
+            if (q > 30) {
+              q = q-30;
+            }
+            strip.setPixelColor(i, colorFade(q,0,0,127,0,0,10);
+          }
+          delay(50);
+          strip.show();
+        }
+        break;
+      //shooting state
+      case 5:
+        for (int i=29; i>=0; i--) {
+          for (int x=0; x<=i; x++) {
+            strip.setPixelColor(i, rainbow(x));
+            strip.setPixelColor(x-1, strip.Color(0,0,0));
+            strip.show();
+            delay(20);
+          }
+        }
+        for (int i=0; i<=13; i++) {
+          for (int x=i; x>=0; x--) {
+            strip.setPixelColor(x, rainbow(i));
+            strip.setPixelColor(x+1, strip.Color(0,0,0));
+            strip.setPixelColor(29-x, rainbow(29-i));
+            strip.setPixelColor(28-x, strip.Color(0,0,0));
+            strip.show();
+            delay(20);
+          }
+          strip.setPixelColor(0,0);
+          strip.setPixelColor(29,0);
+        }
+        break;
+      //lifting state
+      case 6:
+        for (int x=0; x<=29; x++) {
+          strip.setPixelColor(x, strip.Color(127,random(16),random(5)));
+          strip.show();
+          delay(30);
+        }
+        for (int x=0; x<=29; x++) {
+          strip.setPixelColor(29-x, strip.Color(random(15),random(127),127));
+          strip.show();
+          delay(30);
+        }
+        break;
+      //end game disabled state
+      case 7:
+      
+        break;
+    }
+  }
+  //red case
+  else if (team == true) {
+    switch(data[0]) {
+      //default sequence, used for before match
+      case 0:
+        for (int x=0; x<=30; x++) {
+          for (int i=0; i<=30; i++) {
+            int q = i+x;
+            if (q > 30) {
+              q = q-30;
+            }
+            strip.setPixelColor(i, colorFade(q, 127,3,0,70,50,40));
+          }
+        delay(50);
+        strip.show();
+        }
+        break;
+      //auto driving state
       case 1:
         for (int x=0; x<=30; x++)
         {
@@ -88,50 +189,20 @@ void loop() {
         }
         strip.show();
         break;
-      //second autonomous state
+      //finished auto state
       case 2:
-        for (int x=0; x<=30; x++) {
-          movingDot(x, colorFade(x,0,0,127,0,60,60));
-        }
+      
         break;
-      //third autonomous state
+      //teleop driving state
       case 3:
-        for (int x=0; x<=30; x++) {
-          movingDot(x, colorFade(x,0,0,127,0,60,60));
-        }
+      
         break;
-      //fourth autonomous state
+      //collecting state
       case 4:
-        wipeColor(strip.Color(0,0,127));
-        for (int i=0; i<=29; i++) {
-          double color = colorFade(i,127,0,0,0,0,127);
-          for (int x=0; x<=i; x++) {
-            strip.setPixelColor(x, color);
-            strip.show();
-          }
-        }
-        for (int i=0; i<=29; i++) {
-          double color = colorFade(i,127,0,0,0,0,127);
-          for (int x=0; x<=i; x++) {
-            strip.setPixelColor(29-x, color);
-            strip.show();
-          }
-        }
+      
         break;
-      //fifth autonomous state
+      //shooting state
       case 5:
-      
-        break;
-      //sixth autonomous state
-      case 6:
-      
-        break;
-      //seventh autonomous state
-      case 7:
-      
-        break;
-      //eighth autonomous state
-      case 8:
         for (int i=29; i>=0; i--) {
           for (int x=0; x<=i; x++) {
             strip.setPixelColor(i, rainbow(x));
@@ -153,8 +224,8 @@ void loop() {
           strip.setPixelColor(29,0);
         }
         break;
-      //collecting teleop state
-      case 9:
+      //lifting state
+      case 6:
         for (int x=0; x<=29; x++) {
           strip.setPixelColor(x, strip.Color(127,random(16),random(5)));
           strip.show();
@@ -166,128 +237,9 @@ void loop() {
           delay(30);
         }
         break;
-      //shooting teleop state
-      case 10:
-      
-        break;
-      //driving teleop state
-      case 11:
-      
-        break;
-      //lifting teleop state
-      case 12:
-        
-        break;
-      //end of game disabled state
-      case 13:
-        
-        break;
-    }
-  }
-  //red case
-  else if (team == true) {
-    switch(data[0]) {
-      //default sequence, used for before match
-      case 0:
-        wipeColor(strip.Color(0,0,127));
-        for (int i=0; i<=29; i++) {
-          double color = colorFade(i,127,0,0,0,0,127);
-          for (int x=0; x<=i; x++) {
-            strip.setPixelColor(x, color);
-            strip.show();
-          }
-        }
-        for (int i=0; i<=29; i++) {
-          double color = colorFade(i,127,0,0,0,0,127);
-          for (int x=0; x<=i; x++) {
-            strip.setPixelColor(29-x, color);
-            strip.show();
-          }
-        }
-        break;
-      //first autonomous state
-      case 1:
-        for (int x=0; x<=30; x++)
-        {
-          down(x, strip.Color(127,0,0),20);
-        }
-        strip.show();
-        for (int x=0; x<=30; x++)
-        {
-          back(x, strip.Color(127,0,0),20);
-        }
-        strip.show();
-        break;
-      //second autonomous state
-      case 2:
-        for (int x=0; x<=30; x++) {
-          movingDot(x, colorFade(x, 127,3,0,70,50,40));
-        }
-        break;
-      //third autonomous state
-      case 3:
-        for (int x=0; x<=30; x++) {
-          movingDot(x, colorFade(x, 127,3,0,70,50,40));
-        }
-        break;
-      //fourth autonomous state
-      case 4:
-      
-        break;
-      //fifth autonomous state
-      case 5:
-      
-        break;
-      //sixth autonomous state
-      case 6:
-      
-        break;
-      //seventh autonomous state
+      //end game disabled state
       case 7:
       
-        break;
-      //eighth autonomous state
-      case 8:
-        for (int i=29; i>=0; i--) {
-          for (int x=0; x<=i; x++) {
-            strip.setPixelColor(i, rainbow(x));
-            strip.setPixelColor(x-1, strip.Color(0,0,0));
-            strip.show();
-            delay(20);
-          }
-        }
-        for (int i=0; i<=13; i++) {
-          for (int x=i; x>=0; x--) {
-            strip.setPixelColor(x, rainbow(i));
-            strip.setPixelColor(x+1, strip.Color(0,0,0));
-            strip.setPixelColor(29-x, rainbow(29-i));
-            strip.setPixelColor(28-x, strip.Color(0,0,0));
-            strip.show();
-            delay(20);
-          }
-          strip.setPixelColor(0,0);
-          strip.setPixelColor(29,0);
-        }
-        break;
-      //first teleop state
-      case 9:
-      
-        break;
-      //second teleop state
-      case 10:
-      
-        break;
-      //aiming state
-      case 11:
-      
-        break;
-      //lifting teleop state
-      case 12:
-        
-        break;
-      //match end state
-      case 13:
-        
         break;
     }
   }
@@ -325,9 +277,6 @@ Flowing control statement
     delay(50);
     strip.show();
 */
-void setPixel(int i, double color) {
-  strip.setPixelColor(i, color);
-}
 
 void movingDot(int x, double color) {
   strip.setPixelColor(x, color);
