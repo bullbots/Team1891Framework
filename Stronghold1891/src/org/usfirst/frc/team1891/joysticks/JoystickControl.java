6bd036fe2e32c1331f240259761f406b0e3bad26
Josh Taylor <joshtt24@gmail.com>
2016-03-01 20:17:21 -0700

    Added more profiles to joystick class

--- Stronghold1891/src/org/usfirst/frc/team1891/joysticks/JoystickControl.java
+++ Stronghold1891/src/org/usfirst/frc/team1891/joysticks/JoystickControl.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Joystick.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
-import org.usfirst.frc.team1891.filewriter.LogWriter;
 /**
  * @author Josh
  *
@@ -20,7 +19,7 @@ public class JoystickControl {
 	boolean[] on = new boolean[20];
 	int aCount = 0;
 	Timer time = new Timer();
-
+	String profile = "No Supported Controller!!!";
 	Joystick Stick1;
 	Joystick Stick2;
 	/**
@@ -30,13 +29,12 @@ public class JoystickControl {
 
 	/**
 	 * updates all of the data from the joystick and passes it to JoyVector
+	 * @return the data from the joystick package
 	 */
 	public JoyVector getData()
 	{
 		switch(getProfile()){
 		case 0:
-			System.out.println("Not a supported controller!");
-
 			break;
 			//xbox
 		case 1:
@@ -52,15 +50,29 @@ public class JoystickControl {
 			z = 0;
 			angle = getAngle(x,-y);
 			break;
-
+			//dual Logitech Attack
 		case 3:
-
 			x = (joy1Axis(1)-joy2Axis(1))/2;
 			y = (joy1Axis(1)+joy2Axis(1))/2;
 			z = 0;
 			angle = getAngle(x,-y);
-
+			break;
+			//Nyko Airflo
+		case 4:
+			x = joy1Axis(0);
+			y = joy1Axis(1);
+			z = joy1Axis(3);
+			angle = getAngle(x,-y);
+			break;
+			//Logitech Extreme 3D
+		case 5:
+			x = joy1Axis(0);
+			y = joy1Axis(1);
+			z = joy1Axis(2);
+			angle = getAngle(x,-y);
+			break;
 		}
+		SmartDashboard.putString("Controller:", profile);
 		return vector = new JoyVector(x, y, z, angle);
 	}
 
@@ -73,15 +85,15 @@ public class JoystickControl {
 	{
 		Stick1 = Joy;
 		Stick2 = Joy2;
+		clearButtons();
 	}
+	/**
+	 * @param Joy is the index for which joystick you want to select
+	 */
 	public void init(Joystick Joy)
 	{
 		Stick1 = Joy;
-		for(int a = 0; a<20; a++){
-			press[a] = false;
-			release[a] = true;
-			on[a] = false;
-		}
+		clearButtons();
 	}
 
 	/**
@@ -93,6 +105,10 @@ public class JoystickControl {
 		if (Math.abs(Stick1.getRawAxis(axis))>DEADZONE)return Stick1.getRawAxis(axis);
 		return 0;
 	}
+	/**
+	 * @param axis is the axis you want to read
+	 * @return value for an axis
+	 */
 	public double joy2Axis(int axis)
 	{
 		if (Math.abs(Stick2.getRawAxis(axis))>DEADZONE)return Stick2.getRawAxis(axis);
@@ -142,35 +158,64 @@ public class JoystickControl {
 	}
 
 	/**
-	 * This method identifies what type of joystick is plugged in. Xbox 360 is 1. Logitech Attack is 2.
+	 * This method identifies what type of joystick is plugged in.
+	 * XBox is 1
+	 * Single Logitech Attack is 2
+	 * Dual Logitech Attack is 3
+	 * Nyko Airflo is 4
+	 * Logitech Extreme 3D is 5
+	 * CURRENT BUG: Logitech Dual Action is read as a Logitech Extreme 3D
 	 * @return value for joystick type index
 	 */
 	public int getProfile()
 	{
 		//sets the profile to be Xbox (360 or One)
-
-
 		if(Stick1.getAxisCount() == 6 && (Stick1.getButtonCount() == 10||Stick1.getButtonCount() == 11))
 		{
+			profile = "XBox";
 			return 1;
-		}
-		//sets the profile to be single Logitech Attack
-		else if(Stick1.getAxisCount() == 3 && Stick1.getButtonCount() == 11)
-		{
-			return 2;
 		}
 		//sets the profile to be dual Logitech Attack
 		else if(Stick1.getAxisCount() == 3 && Stick1.getButtonCount() == 11 && Stick2.getAxisCount() == 3 && Stick2.getButtonCount() == 11)
 		{
+			profile = "Dual Logitech Attack";
 			return 3;
 		}
-
-
+		//sets the profile to be single Logitech Attack
+		else if(Stick1.getAxisCount() == 3 && Stick1.getButtonCount() == 11)
+		{
+			profile = "Single Logitech Attack";
+			return 2;
+		}
+		//sets the profile to be Nyko Airflo
+		else if(Stick1.getAxisCount() == 4 && Stick1.getButtonCount() == 13)
+		{
+			profile = "Nyko Airflo";
+			return 4;
+		}
+		//sets the profile to be Logitech Extreme 3D
+		else if(Stick1.getAxisCount() == 4 && Stick1.getButtonCount() == 12)
+		{
+			profile = "Logitech Extreme 3D";
+			return 5;
+		}
+		
 		return 0;
 	}
 
+	/**
+	 * Resets the button toggle booleans to default value.
+	 */
+	public void clearButtons(){
+		for(int a = 0;a<20;a++){
+			press[a] = false;
+			release[a] = true;
+			on[a] = false;
+		}
+	}
 
 	/**
+	 * @param i is the button number
 	 * @param index is the button index
 	 * @return whether or not the switch is on
 	 */
